7ec77a272263b99f0054d132683239aa58057b94
Jay Wani <w.m.junaid@gmail.com>
2016-10-01 14:52:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <body>
 <div align="center">
-                <h1>AWS OpsWorks Sample</h1>
+                <h1>AWS OpsWorks Sample.</h1>
                 <h2>This website has been built using AWS OpsWorks. </h2>
                 <h2>Two major components of OpsWorks are Stacks and Layers</h2>
                 <div align="center"><img src="https://s3.ap-south-1.amazonaws.com/opsworkstestbucket2016/index.jpg"></div>
