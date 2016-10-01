7ae0a1b7ed94b37787edf40dbd96f3e81f3cfc0c
Jay Wani <w.m.junaid@gmail.com>
2016-10-01 11:13:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>AWS OpsWorks Sample</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>AWS OpsWorks Sample</h1>
+                <h2>This website has been built using AWS OpsWorks. </h2>
+                <h2>Two major components of OpsWorks are Stacks & Layers</h2>
+                <div align="center"><img src="https://s3.ap-south-1.amazonaws.com/opsworkstestbucket2016/index.jpg"></div>
+                <p>The App that we created is now active & running of &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
